02419b24a9ac9279aca203f821b084169a8c8b54
GakPower <alanigogos@gmail.com>
2020-09-12 18:14:56 +0200

    Environment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mailJS: '18a30a26-ecf6-4f69-8ad7-5ef5d0a9f80f',
+  googleAPI: 'AIzaSyC7klWyRPYZLDm-hRKZuodOSU0K2PhtS90',
+  firebase: {
+    apiKey: 'AIzaSyATmLYHcd_xt0NzB6vLQTAQDf5GIup9UUI',
+    authDomain: 'portfolio-1234567.firebaseapp.com',
+    databaseURL: 'https://portfolio-1234567.firebaseio.com',
+    projectId: 'portfolio-1234567',
+    storageBucket: 'portfolio-1234567.appspot.com',
+    messagingSenderId: '695862845684',
+    appId: '1:695862845684:web:aee2c4a0f188e9abd4bea1',
+    measurementId: 'G-PJ54S2MS66'
+  }
 };
 
 /*
