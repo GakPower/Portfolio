4e9aeef6448cc5c8dcd642381d88573160de2afb
GakPower <alanigogos@gmail.com>
2021-04-22 09:55:19 +0200

    Updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   mailJS: '18a30a26-ecf6-4f69-8ad7-5ef5d0a9f80f',
-  googleAPI: 'AIzaSyC7klWyRPYZLDm-hRKZuodOSU0K2PhtS90',
+  googleAPI: 'AIzaSyATmLYHcd_xt0NzB6vLQTAQDf5GIup9UUI',
   firebase: {
     apiKey: 'AIzaSyATmLYHcd_xt0NzB6vLQTAQDf5GIup9UUI',
     authDomain: 'portfolio-1234567.firebaseapp.com',
